7da582ac28144b177d8e104fdbd90da7c4188d77
Sanan Rao <ninjaboy323@hotmail.com>
2017-10-02 19:08:42 -0600

    Final testing
    Final testing, finalizing and organizing.

--- src/com/sanan/highestnumber/Main.java
+++ src/com/sanan/highestnumber/Main.java
@@ -4,7 +4,7 @@ import com.sanan.highestnumber.util.MathUtil;
 
 public class Main {
 
-    private static String input = "420, 42, 423";
+    private static String input = "99, 97, 120, 7";
 
     public static void main(String[] args) {
         System.out.println(MathUtil.getLargestPossible(input));
